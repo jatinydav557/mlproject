79b6d37bf4f878f723cb0e65cb817bd8001454bc
Jatin Yadav <jatinydav557@gmail.com>
2025-04-25 17:41:17 +0530

    eda and ml

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -19,4 +19,3 @@ class CustomException(Exception):
         return self.error_message
     
 
-        
